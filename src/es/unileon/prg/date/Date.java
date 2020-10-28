92a16f56ed13d857ac61d9ff02b4ffb94494e7ba
smarta03 <smarta03@estudiantes.unileon.es>
2020-10-28 20:57:41 +0100

    Falta: validaciones y isRightDay

---
+++ src/es/unileon/prg/date/Date.java
@@ -0,0 +1,182 @@
+package es.unileon.prg.date;
+
+//La clase publica
+public class Date{
+
+    //Declaracion de variables,los atributos de dentro siempre privados.
+    private int day;
+    private int month;
+    private int year;
+
+    //Constructor de la fecha
+    Date (int day, int month, int year){
+        this.day = day;
+        //this.setDay(day); // se pone asi para llamar a las comprobaciones
+        this.month = month;
+        //this.setMonth(month);
+        this.year = year;
+        //this.setYear(year);
+
+    }
+
+    // void setDay(int day){ //Comprobacion de que es un dia valido
+        
+
+    // }
+
+    // void setMonth(int month){ //Comprobacion de que es un mes valido
+
+
+    // }
+
+    // void setYear(int year){ //Comprobacion de que es un anno valido
+
+
+    // }
+
+    int getDay() {
+
+        return this.day;
+
+    }
+
+    int getMonth(){
+        
+        return this.month;
+
+    }
+
+
+    int getYear(){
+        return this.year;
+    }
+
+
+    //IF
+
+    boolean isSameDay (Date anotherDate){
+
+        return this.day == anotherDate.getDay();
+
+    }
+
+    boolean isSameMonth(Date anotherDate){
+
+        return this.month == anotherDate.getMonth();
+    
+    }
+
+    boolean isSameYear (Date anotherDate){ //Recibe un parametro que es un objeto de tipo fecha (tomorrow) en la priemra llamada y today en la segunda
+        //boolean isSame;
+
+        //if(this.year == anotherDate.getYear()){ //primera llamada this sera today y lo conocemos y another date seria tomorow que hay que pedirselo al metodo que devuelve el anno ya qeu no conocemos el objeto
+            //isSame = true;
+
+
+        //} else {
+            //isSame = false;
+
+        //}
+
+        //Al no tener VOID significa que devuelve algo por l oque annadimos return;
+        //return isSame; //Devolvemos la variable de si es igual
+
+
+        //AHORA SIN IF COMO DICE EL EJERCICIO
+
+        return this.year == anotherDate.getYear();
+
+    }
+
+    boolean isSame(Date anotherDate){
+        boolean isSame;
+        if((this.isSameDay(anotherDate)) && (this.isSameMonth(anotherDate)) && (this.isSameYear(anotherDate))){ //Para no repetir el mismo codigo que el primer if llamamos directamente al metodo
+            isSame = true;
+        } else {
+            isSame = false;
+        }
+
+        return isSame;
+    }
+
+
+    //SWITCH
+
+    String getNameMonth(){
+
+        String name = "";
+
+        switch (this.month){
+            case 1: name = "Enero"; break;
+            case 2: name = "Febrero"; break;                
+            case 3: name = "Marzo"; break;
+            case 4: name = "Abril"; break;
+            case 5: name = "Mayo"; break;
+            case 6: name = "Junio"; break;
+            case 7: name = "Julio"; break;
+            case 8: name = "Agosto"; break;
+            case 9: name = "Septiembre"; break;
+            case 10: name = "Octubre"; break;
+            case 11: name = "Noviembre"; break;
+            case 12: name = "Diciembre"; break;
+            
+            //...
+            //No ponemos el default ya que se comprueba que sea un mes valido en el constructor           
+        }
+        
+        return name;
+    }
+
+    //isRightDay
+
+    String getSeason(){
+
+        String season = "";
+
+        switch (this.month){
+            
+            case 3: 
+                if(this.day>=21){
+                    season = "Primavera";
+                } else{
+                    season = "Invierno";
+                }
+            break;
+
+            case 4: //next
+            case 5: season = "Primavera"; break;
+            case 6:
+                if(this.day<=20){
+                    season = "Primavera";
+                } else {
+                    season = "Verano";
+                }
+            break;
+
+            case 7: //next
+            case 8: season = "Verano"; break;
+            case 9:
+                if(this.day<=20){
+                    season = "Verano";
+                } else {
+                    season = "Otonyo";
+                }
+            break;
+
+            case 10: //next
+            case 11: season = "Otonyo"; break;
+            case 12:
+                if(this.day<=20){
+                    season = "Otonyo";
+                } else {
+                    season = "Invierno";
+                }
+            break;
+
+            case 1: //next
+            case 2: season = "Invierno";
+        }
+
+        return season;
+    }
+}
---
+++ src/es/unileon/prg/date/MainDate.java
@@ -0,0 +1,30 @@
+package es.unileon.prg.date;
+
+public class MainDate{
+
+    public static void main (String args[]){
+
+        Date today,tomorrow; //Creamos objetos de tipo fecha para compararlos
+
+        today = new Date(21,12,2020); //Llamada al contructor de la fecha que se llama desde Date y se le pasan directamente los parametros
+        tomorrow = new Date(21,12,2020);
+
+        
+        System.out.println("El mes de today es: " + today.getNameMonth()); //No se le pasa parametros porque ya se pasa el objeto today
+        System.out.println("El mes de tomorrow es: " + tomorrow.getNameMonth());
+        System.out.println("La estacion de today es: " + today.getSeason());
+        System.out.println("La estacion de tomorrow es: " + tomorrow.getSeason());
+
+        //COMPARAR DIA
+        System.out.println("today.isSameDay(tomorrow): " + today.isSameDay(tomorrow));
+        //COMPARA MONTH
+        System.out.println("today.isSameMonth(tomorrow): " + today.isSameMonth(tomorrow));
+        //COMPARA YEAR
+        System.out.println("today.isSameYear(tomorrow): " + today.isSameYear(tomorrow));
+        //o tomorrow.isSameYear(today); es lo mismo ya que compara lo mismo
+        System.out.println("tomorrow.isSameYear(today): " + tomorrow.isSameYear(today));
+        //COMPARA FECHA
+        System.out.println("today.isSameYear(tomorrow): " + today.isSame(tomorrow));
+    }
+    
+}
